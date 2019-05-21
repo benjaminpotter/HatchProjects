82af18d80d897f987b99340c4f052a625e849a76
Benjamin <reactiveneon@gmail.com>
2019-05-21 14:53:28 -0400

    simultaneous circles

---
+++ Week 3/Simultaneous Circles/challenge1.py
@@ -0,0 +1,24 @@
+def setup():
+    size(400, 400)
+
+max_distance = 400
+
+def paint_circles():
+    global max_distance
+    
+    x = -1
+    while x < width:
+        y = -1
+        while y < height:
+            circle_size = dist(mouseX, mouseY, x, y)
+            circle_size = (circle_size/max_distance) * 66
+            fill(100, 150, 200)
+            ellipse(x, y, circle_size, circle_size)
+            y += 20
+        x += 20
+        
+def draw():
+    background(51)
+    
+    noStroke()
+    paint_circles()
