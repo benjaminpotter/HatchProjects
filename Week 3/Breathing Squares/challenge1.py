c72b4f6801720a5495673541c42f814e28c5e38f
Benjamin <reactiveneon@gmail.com>
2019-05-29 13:05:10 -0400

    breathing squares

---
+++ Week 3/Breathing Squares/challenge1.py
@@ -0,0 +1,43 @@
+def setup():
+    size(400, 400)
+
+rotation = 0
+speed = 0.01
+squares_on = True
+
+def outer_squares():
+    popMatrix()
+    fill(102, 179, 164)
+    rect(0, 0, 175, 175)
+    rect(225, 0, 175, 175)
+    rect(0, 225, 175, 175)
+    rect(225, 225, 175, 175)
+
+def inner_square():
+    global speed, rotation
+    
+    fill(102, 57, 40)
+    translate(200, 200)
+    rotation += speed
+    rotate(rotation)
+    rect(-125, -125, 250, 250)
+    
+def draw():
+    global squares_on
+    
+    background(100, 150, 200)
+    noStroke()
+    if squares_on:
+        pushMatrix()
+    inner_square()
+    
+    if squares_on:
+        outer_squares()
+        
+def keyPressed():
+    global squares_on
+    
+    if key == ' ' and squares_on == True:
+        squares_on = False
+    elif key == ' ':
+        squares_on = True
---
+++ Week 3/Breathing Squares/challenge3.py
@@ -0,0 +1,55 @@
+def setup():
+    size(400, 400)
+
+rotation1 = 0
+rotation2 = 0
+speed = 0.01
+squares_on = True
+colour = color(random(255), random(255), random(255))
+
+def outer_squares():
+    global colour
+    
+    popMatrix()
+    fill(colour)
+    rect(0, 0, 175, 175)
+    rect(225, 0, 175, 175)
+    rect(0, 225, 175, 175)
+    rect(225, 225, 175, 175)
+
+def inner_square():
+    global speed, rotation1, rotation2
+    
+    fill(102, 57, 40)
+    translate(200, 200)
+    rotation1 += speed
+    rotate(rotation1)
+    rect(-125, -125, 250, 250)
+    
+    fill(152, 107, 90)
+    rotation2 += speed + 0.01
+    rotate(rotation2)
+    rect(-100, -100, 200, 200)
+    
+def draw():
+    global squares_on
+    
+    background(100, 150, 200)
+    noStroke()
+    if squares_on:
+        pushMatrix()
+    inner_square()
+    
+    if squares_on:
+        outer_squares()
+        
+def keyPressed():
+    global squares_on, colour
+    
+    if key == ' ':
+        colour = color(random(255), random(255), random(255))
+        
+        if squares_on == True:
+            squares_on = False
+        else:
+            squares_on = True
