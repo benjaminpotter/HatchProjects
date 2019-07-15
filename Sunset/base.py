60840f2b17709f4abb61eefe823aa32811992d46
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-07-15 15:51:17 -0400

    Create base.py

---
+++ Sunset/base.py
@@ -0,0 +1,13 @@
+def setup():
+	size(400, 400)
+sunset = 0
+speed = 0.5
+def draw() :
+  sunset += speed
+  background(0, 255, 221)
+  fill(255, 221, 0)
+  ellipse(300, 20 + sunset, 50, 50)
+  fill(80, 219, 0)
+  rect(0, 200, 400, 200)
+  fill(0, 0, 0, sunset * 1.2)
+  rect(0, 0, 400, 400)
