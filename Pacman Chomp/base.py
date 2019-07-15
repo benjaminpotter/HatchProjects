0729c5285badec478086b6d805827db1e8f4f524
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-07-15 10:20:45 -0400

    Create base.py

---
+++ Pacman Chomp/base.py
@@ -0,0 +1,16 @@
+def setup():
+      size(400, 400)
+setup()
+chomp_speed = 5
+pac_mouth = 1
+pac_mouth_close = 0
+
+def draw():
+    background(0)
+    fill(255, 255, 0)
+    pac_mouth_close = 360 - pac_mouth
+    arc(200, 200, 80, 80, pac_mouth, pac_mouth_close)
+    if pac_mouth >= 45 || pac_mouth <= 0 :
+        chomp_speed *= -1
+
+pac_mouth += chomp_speed
