17e353be380643e6df7aa4e2e26c1f3fbc90b41b
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-07-15 13:25:03 -0400

    Create base.py

---
+++ Brickbreaker Paddle/base.py
@@ -0,0 +1,16 @@
+def setup():
+	size(400, 400)
+Paddle = :
+  x: 10,
+  y: 373,
+  height: 17,
+  width: 81
+
+def moving_paddle() :
+  fill(18, 6, 18)
+  rect(Paddle.x, Paddle.y, Paddle.width, Paddle.height)
+  Paddle.x = mouseX - (Paddle.width / 2)
+
+def draw():
+  background(232, 255, 248)
+  moving_paddle()
