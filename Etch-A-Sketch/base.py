17d3fba31e43840be7ce2536094652673c5a6939
Rishi <rishi30.mehta@gmail.com>
2019-07-17 10:57:48 -0400

    Update base.py

--- Etch-A-Sketch/base.py
+++ Etch-A-Sketch/base.py
@@ -1,23 +1,23 @@
-x = 200;
-y = 200;
-speed = 5;
+x = 200
+y = 200
+speed = 5
 
 
 def setup():
     size(400, 400)
-    textAlign(CENTER, CENTER);
+    textAlign(CENTER, CENTER)
     drawBackground()
 
 def drawBackground():
-    background(255, 0, 0);
-    fill(184, 167, 167);
-    rect(25, 25, 350, 300);
-    fill(255, 255, 255);
-    ellipse(50, 360, 50, 50);
-    ellipse(350, 360, 50, 50);
-    fill(221, 255, 0);
-    textSize(30);
-    text("ETCH A SKETCH", 200, 360);
+    background(255, 0, 0)
+    fill(184, 167, 167)
+    rect(25, 25, 350, 300)
+    fill(255, 255, 255)
+    ellipse(50, 360, 50, 50)
+    ellipse(350, 360, 50, 50)
+    fill(221, 255, 0)
+    textSize(30)
+    text("ETCH A SKETCH", 200, 360)
     
 def draw(): 
     fill (0, 0, 0)
