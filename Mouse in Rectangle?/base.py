b901ffd1fbf8813e173ff944ce2c2d53e2482afd
Benjamin <reactiveneon@gmail.com>
2019-07-17 15:26:41 -0400

    projects

--- Mouse in Rectangle?/base.py
+++ Mouse in Rectangle?/base.py
@@ -1,23 +1,16 @@
-def setup():
-    size(400, 400)
-    
-rect_colour = color(255, 0, 0)
+rectangleColor = color(255, 0, 0)
+def checkMousePosition():
+  if mouseX > 100 and mouseX < 300 and mouseY > 150 anda mouseY < 250 :
+    rectangleColor = color(0, 255, 0)
+  else :
+    rectangleColor = color(255, 0, 0)
 
-def check_mouse_position():
-    global rect_colour
-    
-    if mouseX > 100 and mouseX < 300 and mouseY > 150 and mouseY < 250:
-        rect_colour = color(0, 255, 0)
-    else:
-        rect_colour = color(255, 0, 0)
-        
-def draw_rect():
-    global rect_colour
-    fill(rect_colour)
-    rect(100, 150, 200, 100)
-    
+def drawRectangle():
+  fill(rectangleColor)
+  rect(100, 150, 200, 100)
+
 def mouseMoved():
-    check_mouse_position()
-        
+  checkMousePosition()
+
 def draw():
-    draw_rect()
+  drawRectangle()
