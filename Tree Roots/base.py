78e2db3c86719d564d2280e8f8aed6288c330add
Rishi <rishi30.mehta@gmail.com>
2019-07-18 10:46:47 -0400

    Create base.py

---
+++ Tree Roots/base.py
@@ -0,0 +1,26 @@
+def drawLandscape():
+    background(135, 206, 250);
+    fill(151, 107, 83)
+    rect(0, 300, 400, 100)
+    fill(30, 137, 30)
+    rect(0, 280, 400, 20)
+    
+drawLandscape()
+
+def drawTree():
+    fill(101, 67, 33)
+    rect(180, 100, 40, 200)
+    fill(30, 137, 30)
+    ellipse(100, 100, 100, 100)
+    ellipse(200, 100, 100, 100)
+    ellipse(300, 100, 100,  100)
+    ellipse(165, 55, 100, 100)
+    ellipse(235, 55, 100, 100)
+    ellipse(160, 120, 100, 100)
+    ellipse(240, 120, 100, 100)
+    fill(101, 67, 33)
+    triangle(180, 300, 220, 300, 350, 350)
+    triangle(180, 300, 220, 300, 75, 350)
+    triangle(210, 305, 225, 310, 200, 350)
+
+drawTree();
