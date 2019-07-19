748b02df0e51b0fddcbf9da8b35ab559e82eeb65
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-07-19 12:01:08 -0400

    Update challenge1.py

--- Arc Drawing/challenge1.py
+++ Arc Drawing/challenge1.py
@@ -1,6 +1,5 @@
-var mouseDragged = function(){
-    noFill();
-    ellipseMode(CORNER);
-    stroke(random(mouseX, mouseY), random(mouseX, mouseY), random(102,200));
-    arc(0, 0, mouseX, mouseY, 0, mouseY);
-};
+def mouseDragged():
+    noFill()
+    ellipseMode(CORNER)
+    stroke(random(mouseX, mouseY), random(mouseX, mouseY), random(102,200))
+    arc(0, 0, mouseX, mouseY, 0, mouseY)
