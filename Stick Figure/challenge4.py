a43931fb1d66366b83584510e62842504b4f3c4f
Rishi <rishi30.mehta@gmail.com>
2019-07-23 13:52:35 -0400

    Create challenge4.py

---
+++ Stick Figure/challenge4.py
@@ -0,0 +1,27 @@
+stroke(0, 0, 0)
+strokeWeight(4)
+def drawOutLine():
+    fill(255, 224, 189)
+    ellipse(195, 90, 105, 105)
+    line(195, 143, 192, 277)
+    line(136, 372, 191, 276)
+    line(195, 277, 248, 370)
+    line(193, 147, 135, 236)
+    line(195, 145, 255, 238)
+drawOutLine()
+def drawFace():
+   point(175, 75)  
+   point(215, 75)
+   noFill()
+   arc(195, 105, 40, 20, 20, 160)
+drawFace()
+def drawSpeech():
+   strokeWeight(1)
+   noStroke()
+   fill(230)
+   rect(260, 50, 120, 50, 40)
+   triangle(260, 80, 260, 120, 300, 80)
+   fill(0)
+   text("Hello!", 300, 80)
+drawSpeech()
+
