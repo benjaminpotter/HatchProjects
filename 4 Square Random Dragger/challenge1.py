5fe738b8fecb36d2e86c04b1ad6eca2f0100ed65
ReactiveNeon <39702521+ReactiveNeon@users.noreply.github.com>
2019-07-23 13:32:55 -0400

    Create challenge1.py

---
+++ 4 Square Random Dragger/challenge1.py
@@ -0,0 +1,36 @@
+background(255)
+
+last_square = 0
+current_square = 0
+
+def setup_scene(): 
+    strokeWeight(5)
+    line(200, 0, 200, 400)
+    line(0, 200, 400, 200)
+setup_scene()
+    
+def change_colour():
+    global last_square, current_square
+    
+    fill(random(0, 255), random(0, 255), random(0, 255))
+    last_square = current_square
+
+def draw_squares():
+    global current_square
+    
+    if mouseX < 200 and mouseY < 200: 
+        rect(0, 0, 200, 200)
+        current_square = 1
+    elif mouseX < 200 and mouseY > 200:
+        rect(0, 200, 200, 200)
+        current_square = 2
+    elif mouseX > 200 and mouseY < 200:
+        rect(200, 0, 200, 200)
+        current_square = 3
+    elif mouseX > 200 and mouseY > 200:
+        rect(200, 200, 200, 200)
+        current_square = 4
+        
+def mousePressed(e):
+    change_colour()
+    draw_squares()
