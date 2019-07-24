0fbfbcbfca9584ae248d9960c0227392345465ee
ReactiveNeon <39702521+ReactiveNeon@users.noreply.github.com>
2019-07-24 09:09:57 -0400

    Create challenge1.py

---
+++ Encode a Message/challenge1.py
@@ -0,0 +1,16 @@
+def encode(string):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    string = string.lower()
+    array = list(string)
+    for i in range(len(array)):
+        for a in range(len(alphabet)):
+            if array[i] == alphabet[a]:
+                nextLetter = (a + 2) % 26
+                array[i] = alphabet[nextLetter]
+                break
+    return "".join(array)
+
+textSize(40)
+fill(255, 187, 0)
+textAlign(CENTER, CENTER)
+text(encode("hello world"), 200, 200)
