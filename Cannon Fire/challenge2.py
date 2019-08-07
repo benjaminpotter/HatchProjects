27a8936c45966bf6a244560ac5318645de5c2796
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-07 13:19:32 -0400

    Create challenge2.py

---
+++ Cannon Fire/challenge2.py
@@ -0,0 +1,33 @@
+x = 100
+y = 0
+fire = 0
+noStroke()
+
+def drawCannon():
+   fill(230, 225, 230)
+   rect(0, 256, 400, 150)
+   fill(87)
+   translate(76, 219)
+   rotate(161)
+   ellipse(0, 0, 139, 79)
+   quad(-33, 35, -75, 24, -78, -17, -47, -29)
+   resetMatrix()
+   fill(170)
+   ellipse(60, 250, 70, 70)
+
+def fireCannonBall():
+   if fire :
+   x += 10
+   y = 400 - (((-1 / 279) * Math.pow((x - 231), 2)) + 235)
+   fill(0, 0, 0)
+   fill(0, 0, 0)
+   if x > 365 :
+       x = 365
+       
+def draw():
+   background(235, 241, 255)
+   fireCannonBall()
+   drawCannon()
+
+def mouseClicked():
+   fire = 1
