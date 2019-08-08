bc764a07a83c7d15c17aa08347f7630248b35115
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-08 11:15:59 -0400

    Create challenge2.py

---
+++ Sunset/challenge2.py
@@ -0,0 +1,15 @@
+sunset = 0
+speed = 0.5
+
+def draw():
+   global sunset, speed
+   sunset += speed
+   background(0, 255, 221)
+   fill(255, 221, 0)
+   ellipse(300 + sunset, 20 + sunset, 50, 50)
+   fill(80, 219, 0)
+   rect(0, 200, 400, 200)
+   fill(220, 0, 0, sunset * 1.2)
+   rect(0, 0, 400, 200)
+   fill(0, 0, 0, sunset * 1.2)
+   rect(0, 0, 400, 400)
