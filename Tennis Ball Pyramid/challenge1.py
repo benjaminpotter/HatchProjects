d565d7810267ba60f7bb3c2fb751945935d1132f
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-09 10:22:24 -0400

    Create challenge1.py

---
+++ Tennis Ball Pyramid/challenge1.py
@@ -0,0 +1,20 @@
+numRows = 11
+radius = (400 / numRows) / 2
+translateX = radius
+translateY = sqrt(3) * radius
+
+def setupDraw():
+   background(144, 169, 209)
+   strokeWeight(3)
+   fill(0, 255, 30)
+setupDraw()
+
+def drawTennisBalls():
+   for i in range (numRows, 0, -1) :
+       for j in range (0, i) :
+           x = radius * (j * 2 + 1)
+           ellipse(x, 400 - radius, radius * 2, radius * 2)
+      
+   translate(translateX, -translateY)
+
+drawTennisBalls()
