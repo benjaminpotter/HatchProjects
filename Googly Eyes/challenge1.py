fc5b2a84e2ef882b18aaef1e3132a3f8410ed352
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-09 09:57:05 -0400

    Rename Googly Eyes to Googly Eyes/challenge1.py

---
+++ Googly Eyes/challenge1.py
@@ -0,0 +1,19 @@
+eyeX = 120
+eyeY = 160
+
+def drawEyes():
+   fill(255)
+   ellipse(120, 160, 100, 100)
+   ellipse(280, 160, 100, 100)
+   fill(0, 0, 200)
+   ellipse(eyeX, eyeY, 40, 40)
+   ellipse(eyeX + 160, eyeY, 40, 40)
+
+def eyeMovement():
+   eyeX = map(mouseX, 0, 400, 100, 150)
+   eyeY = map(mouseY, 0, 400, 140, 190)
+
+def draw():
+   background(100, 100, 100)
+   drawEyes()
+   eyeMovement()
