4e595e7f23ca022b0e0153d8fb95f3e7a7b36fef
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-09 09:53:47 -0400

    Create challenge1.py

---
+++ Sunrise and Sunset/challenge1.py
@@ -0,0 +1,15 @@
+sunsetX = 0
+sunsetY = 0
+speed = 0.5
+
+def draw():
+   global sunsetX, sunsetY, speed
+   sunsetX += speed
+   sunsetY = 200 + sin(sunsetX / 2) * -200
+   background(0, 255, 221)
+   fill(255, 221, 0)
+   ellipse(sunsetX, sunsetY, 50, 50)
+   fill(80, 219, 0)
+   rect(0, 200, 400, 200)
+   fill(200, 0, 0, sunsetY)
+   rect(0, 0, 400, 200)
