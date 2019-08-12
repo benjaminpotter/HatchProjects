332556a478ce65c833b371bb3bf2abbd58bef360
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-12 10:23:01 -0400

    Create challenge2.py

---
+++ Tennis Ball/challenge2.py
@@ -0,0 +1,14 @@
+def drawBall():
+   background(109, 189, 199)
+   noStroke()
+   fill(255)
+   ellipse(200, 200, 300, 300)
+drawBall()
+
+def drawStitches():
+   stroke(255, 0, 0)
+   strokeWeight(10)
+   noFill()
+   arc(0, 200, 275, 350, 322, 399)   
+   arc(400, 200, 275, 350, 142, 219)
+drawStitches()
