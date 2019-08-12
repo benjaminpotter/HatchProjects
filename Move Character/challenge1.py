4db84e5e1a0dd1a4022e4e9fa6dbe45a397bc2ca
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-12 10:00:22 -0400

    Create challenge1.py

---
+++ Move Character/challenge1.py
@@ -0,0 +1,17 @@
+ySpeed = 5
+yPos = random(0, 400)
+img = getImage("avatars/old-spice-man-blue")
+
+def drawCharacter():
+   image(img, 200, yPos)
+
+def moveCharacter():
+   if keyPressed and keyCode === UP_ARROW :
+       yPos = yPos - ySpeed
+   if keyPressed and keyCode === DOWN_ARROW :
+       yPos = yPos + ySpeed
+
+def draw():
+   background(255, 255, 255)
+   drawCharacter()
+   moveCharacter()
