3cdd83eb14f5808417f1e171663d83e533c2e343
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-12 08:43:36 -0400

    Create challenge2.py

---
+++ Soccer Ball/challenge2.py
@@ -0,0 +1,18 @@
+def drawBall():
+   noStroke();
+   fill(150, 142, 142);
+   ellipse(200, 290, 200, 30);
+   fill(0, 0, 0);
+   ellipse(200, 200, 200, 200)
+drawBall()
+
+def drawPatches():
+   fill(255, 255, 255)
+   translate(200, 200)
+   scale(0.7)
+   for i in range (1, 5) :
+       rotate(72)
+       quad(-25, -35, -25, -95, 25, -95, 25, -35)
+       triangle(-24, -35, -24, -95, -46, -65)
+       triangle(24, -35, 24, -95, 46, -65)
+drawPatches()
