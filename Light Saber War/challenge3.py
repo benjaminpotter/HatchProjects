6030a9dcd9432c9a3f421df3b54ff94fc325ea34
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-12 10:17:04 -0400

    Create challenge3.py

---
+++ Light Saber War/challenge3.py
@@ -0,0 +1,16 @@
+def draw():
+   frameRate(20)
+   noStroke()
+   fill(0, 0, 0, 100)
+   rect(0, 0, 400, 400)
+   stroke(0, 255, 0, mouseY)
+   strokeWeight(20)
+   line(mouseX, mouseY, pmouseX, pmouseY / 3)
+   stroke(255, 0, 0, 400 - mouseY)
+   line(400 - mouseX, 400 - mouseY, 400 - pmouseX, 120 - pmouseY/3)
+   stroke(0, 0, 255, mouseX)
+   line(mouseY, mouseX, pmouseY, pmouseX / 3)
+   if dist(mouseX, mouseY, 200, 200) < 30 :
+       noStroke()
+       fill(255, 255, 0, 200)
+       rect(0, 0, 400, 400)
