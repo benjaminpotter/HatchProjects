5a0022166ea93bbf50f37dcd467dd60bfe15b7ec
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-12 13:15:35 -0400

    Create challenge3.py

---
+++ X-Men Logo/challenge3.py
@@ -0,0 +1,14 @@
+def drawCircle():
+    noStroke()
+    fill(0, 0, 0)
+    ellipse(200, 200, 300, 300)
+    fill(210, 0, 0)
+    ellipse(200, 200, 250, 250)
+drawCircle()
+
+def drawH():
+   fill(0, 0, 0)
+   rect(120, 80, 30, 240)
+   rect(240, 80, 30, 240)
+   rect(120, 190, 130, 30)
+drawH()
