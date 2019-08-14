0009744255647968685a9f082238abc95b45f160
ninakhera <47437770+ninakhera@users.noreply.github.com>
2019-08-14 09:58:46 -0400

    Create challenge2.py

---
+++ Scribbles/challenge2.py
@@ -0,0 +1,6 @@
+def mouseDragged():
+   height = 30
+   width = 15
+   for i in range (0, 5) :
+       stroke(100, random(170, 200), random(200, 255))
+       line(mouseX + random(int(-width), 0), mouseY + random(int(-height), int(height)), mouseX + random(0, int(width)), mouseY + (random(int(-height), int(height))))
